e5aa6d816e99d78bdde04ac8d464dac6e6d5614a
Thoisana <thoisana.keisham@infosys.com>
2025-08-09 23:27:49 +0530

    remaining q from same topic

---
+++ DSA_One/src/_04_Reverse_Array.java
@@ -0,0 +1,26 @@
+public class _04_Reverse_Array {
+    public static int[] reverseArray(int[] arr){
+        int i = 0, j = arr.length-1;
+        while (i <= j){
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++; j--;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,10};
+        System.out.println("=== ARRAY BEFORE REVERSE ===");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(" "+arr[i]);
+        }
+        System.out.println();
+        int[] res = reverseArray(arr);
+        System.out.println("=== REVERSE ARRAY ===");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
---
+++ DSA_One/src/_05_Array_Rotation.java
@@ -0,0 +1,34 @@
+public class _05_Array_Rotation {
+    public static int[] reverseArray(int[] arr, int start, int end){
+        while (start <= end ){
+            int x = arr[start];
+            arr[start] = arr[end];
+            arr[end] = x;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+    public static int[] rotateArray(int[] arr, int k){
+        int n = arr.length;
+        int[] res = reverseArray(arr, 0, n-1);
+        res = reverseArray(res, 0, k-1);
+        res = reverseArray(res, k,n-1);
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int k = 2;
+        System.out.println("--- ARRAY BEFORE ROTATION ---");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(" "+arr[i]);
+        }
+        int[] res  = rotateArray(arr, k);
+        System.out.println();
+        System.out.println("--- Rotated Array ---");
+        for (int i = 0; i< arr.length; i++){
+            System.out.print(" "+arr[i]);
+        }
+
+    }
+}
